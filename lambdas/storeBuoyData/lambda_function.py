cb1c54c249acf6d746b3a5fac7b983279ba08eda
bn4crt <bn4crt@bolton.ac.uk>
2025-04-22 09:14:24 +0100

    🔧 Fix: Correct SNS topic ARN for alerts

--- lambdas/storeBuoyData/lambda_function.py
+++ lambdas/storeBuoyData/lambda_function.py
@@ -13,7 +13,7 @@ s3 = boto3.client('s3')
 bucket_name = 'oes-buoy-storage-bilal'
 
 sns = boto3.client('sns')
-sns_topic_arn = 'arn:aws:sns:eu-north-1:123456789012:BuoyAlerts'
+sns_topic_arn = 'arn:aws:sns:eu-north-1:600627365021:BuoyAlerts'
 
 
 def lambda_handler(event, context):
